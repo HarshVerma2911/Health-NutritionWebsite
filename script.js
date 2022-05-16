f59d7e6773544eadd490188da9d0f0ddc77e1a33
HarshVerma <rharsh242@gmail.com>
2022-05-17 01:43:17 +0530

    linkedeverypage

--- script.js
+++ script.js
@@ -62,44 +62,25 @@ var swiper = new Swiper(".product-slider", {
     },
 });
 
-var swiper = new Swiper(".review-slider", {
-    loop:true,
-    spaceBetween: 20,
-    autoplay: {
-        delay: 7500,
-        disableOnInteraction: false,
-    },
-    centeredSlides: true,
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      1020: {
-        slidesPerView: 3,
-      },
-    },
-});
-
 const ratings = document.querySelectorAll('.rating')
 const ratingsContainer = document.querySelector('.ratings-container')
 const sendBtn = document.querySelector('#send')
 const panel = document.querySelector('#panel')
-let selectedRating = 'Satisfied'
+// let selectedRating = 'Satisfied'
 
 ratingsContainer.addEventListener('click', (e) => {
     if(e.target.parentNode.classList.contains('rating')) {
         removeActive()
         e.target.parentNode.classList.add('active')
         selectedRating = e.target.nextElementSibling.innerHTML
+        console.log(selectedRating,"first")
     }
-    if(e.target.classList.contains('rating')) {
-        removeActive()
-        e.target.classList.add('active')
-        selectedRating = e.target.nextElementSibling.innerHTML
-    }
+    // if(e.target.classList.contains('rating')) {
+    //     removeActive()
+    //     e.target.classList.add('active')
+    //     selectedRating = e.target.nextElementSibling.innerHTML
+    //     console.log(selectedRating,"second")
+    // }
 
 })
 
